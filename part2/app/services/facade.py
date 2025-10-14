84ebc5d3fe22b005c1fe2c370e92f8569ee0e8bc
shihuaxie <sylvia501628@gmail.com>
2025-10-14 14:25:09 +1100

    Feat: complete user service for BL

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -1,8 +1,10 @@
 from app.persistence.repository import InMemoryRepository
+from app.services.user_service import UserService
+
 
 class HBnBFacade:
     def __init__(self):
-        self.user_repo = InMemoryRepository()
+        # self.user_repo = InMemoryRepository()
         self.place_repo = InMemoryRepository()
         self.review_repo = InMemoryRepository()
         self.amenity_repo = InMemoryRepository()
@@ -12,7 +14,8 @@ class HBnBFacade:
     # Placeholder method for creating a user
     def create_user(self, user_data):
         # Logic will be implemented in later tasks
-        pass
+        return self.user_service.create_user(user_data)
+    
 
     # Placeholder method for fetching a place by ID
     def get_place(self, place_id):
