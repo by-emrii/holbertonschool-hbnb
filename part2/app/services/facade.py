b1fe38d89dadcd1c84f0e3c386ae893cb725ed79
“cryspycreme” <crystalchiamm@gmail.com>
2025-10-15 13:23:38 +1100

    Add amenity_service BL and update facade for amenity CRU

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -1,15 +1,16 @@
 from app.persistence.repository import InMemoryRepository
 from app.services.user_service import UserService
-
+from app.services.amenity_service import AmenityService
 
 class HBnBFacade:
     def __init__(self):
         # self.user_repo = InMemoryRepository()
         self.place_repo = InMemoryRepository()
         self.review_repo = InMemoryRepository()
-        self.amenity_repo = InMemoryRepository()
+        # self.amenity_repo = InMemoryRepository()
 
         self.user_service = UserService()
+        self.amenity_service = AmenityService()
 
     # Placeholder method for creating a user
     def create_user(self, user_data):
@@ -32,3 +33,20 @@ class HBnBFacade:
     def get_place(self, place_id):
         # Logic will be implemented in later tasks
         pass
+
+    """ Amenity CRU """
+    # create amenity
+    def create_amenity(self, amenity_data):
+        return self.amenity_service.create_amenity(amenity_data)
+
+    # get amenity
+    def get_amenity(self, amenity_id):
+        return self.amenity_service.get_amenity(amenity_id)
+    
+    # get all amenities
+    def get_all_amenities(self):
+        return self.amenity_service.get_all_amenities()
+    
+    # update amenity
+    def update_amenity(self, amenity_id, amenity_data):
+        return self.amenity_service.update_amenity(amenity_id, amenity_data)
