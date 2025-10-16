2b64e87cd9ebb9e864cf6b7017a7fdcd4be1d0cf
GraceKayem <11557@holbertonstudents.com>
2025-10-17 00:50:34 +1100

    Update facade.py -- review

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -15,7 +15,8 @@ class HBnBFacade:
         self.amenity_service = AmenityService()
         self.reservation_service = ReservationService()
         self.place_service = PlaceService()
-
+        self.review_service = ReviewService()
+        
     """ User CRU """
     # Placeholder method for creating a user
     def create_user(self, user_data):
@@ -80,3 +81,53 @@ class HBnBFacade:
     # update an existing reservation
     def update_reservation(self, reservation_id, reservation_data):
         return self.reservation_service.update_reservation(reservation_id, reservation_data)
+
+"""Review CRU"""
+def create_review(self, review_data):
+        """Create and save a review."""
+        try:
+            return self.review_service.create_review(review_data)
+        except ValueError as error:
+            #in the case of empty comments and invalid rating
+            return {"error":str(error)}
+        except Exception as error:
+            return {"error": f"Unexpected error: {str(error)}"}
+
+    def get_reviews_by_user(self, user_id):
+        """Fetch all reviews made by a specific user."""
+        return self.review_service.get_reviews_by_user(user_id)
+
+    def get_reviews_for_place(self, place_id):
+        """Fetch all reviews for a specific place."""
+        return self.review_service.get_reviews_for_place(place_id)
+    
+    def update_review(self, review_update):
+        """User updates a review of a specific place"""
+        try:
+            review_id = review_update.get("review_id")
+            review_data = review_update.get("review_data")
+            current_user_id = review_update.get("current_user_id")
+            return self.review_service.update_review(review_id, review_data, current_user_id)
+        except ValueError as error:
+            return {"error": str(error)}
+        except PermissionError as error:
+            return {"error": str(error)}
+        except Exception as error:
+            return{"error": f"Unexpected error: {str(error)}"}
+        
+    def get_average_rating(self, place_id):
+        """Calculate satistics rating"""
+        return self.review_service.get_average_rating(place_id)
+    
+    def get_recent_reviews(self, place_id, limit=5):
+        """Recent reviews displayed"""
+        return self.review_service.get_recent_reviews(place_id, limit)
+
+    def delete_review(self, review_id):
+        """Delete a review by ID."""
+        try:
+            return self.review_service.delete_review(review_id)
+        except ValueError as error:
+            return {"error": str(error)}
+        except Exception as error:
+            return {"error": f"Unexpected error: {str(error)}"}
