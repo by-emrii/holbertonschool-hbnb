182cb45e7495937bf910ff91a5f17bc133c207c6
Emily Chew <emilychewsh@gmail.com>
2025-10-16 13:08:57 +1100

    Add reservation CRUD in facade.py

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -1,6 +1,8 @@
 from app.persistence.repository import InMemoryRepository
 from app.services.user_service import UserService
 from app.services.amenity_service import AmenityService
+from app.services.reservation_service import ReservationService
+
 
 class HBnBFacade:
     def __init__(self):
@@ -11,7 +13,9 @@ class HBnBFacade:
 
         self.user_service = UserService()
         self.amenity_service = AmenityService()
+        self.reservation_service = ReservationService()
 
+    """ User CRU """
     # Placeholder method for creating a user
     def create_user(self, user_data):
         # Logic will be implemented in later tasks
@@ -29,6 +33,7 @@ class HBnBFacade:
     def update_user(self, user_id, user_data):
         return self.user_service.update_user(user_id, user_data)
 
+    """ Place CRU """
     # Placeholder method for fetching a place by ID
     def get_place(self, place_id):
         # Logic will be implemented in later tasks
@@ -50,3 +55,20 @@ class HBnBFacade:
     # update amenity
     def update_amenity(self, amenity_id, amenity_data):
         return self.amenity_service.update_amenity(amenity_id, amenity_data)
+
+    """ Reservation CRU """
+    # create reservation
+    def create_reservation(self, reservation_data):
+        return self.reservation_service.create_reservation(reservation_data)
+
+    # get one reservation by ID
+    def get_reservation(self, reservation_id):
+        return self.reservation_service.get_reservation(reservation_id)
+
+    # get all reservations
+    def get_all_reservations(self):
+        return self.reservation_service.get_all_reservations()
+
+    # update an existing reservation
+    def update_reservation(self, reservation_id, reservation_data):
+        return self.reservation_service.update_reservation(reservation_id, reservation_data)
