2c34257d556dabd4f1705bb0d1a8d96012b8d79b
Emily Chew <emilychewsh@gmail.com>
2025-11-03 12:03:50 +1100

    update basemodel with db

--- part3/app/models/base_model.py
+++ part3/app/models/base_model.py
@@ -1,15 +1,17 @@
+from app import db
 import uuid
 from datetime import datetime
 
 
-class BaseModel:
+class BaseModel(db.Model):
     """
     Base Model provides attibutes and methods for all entities
     """
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    __abstract__ = True  # This ensures SQLAlchemy does not create a table for BaseModel
+    
+    id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
     # update method
     def update(self, data = None):
