e3046bb24ae9433f6602aab6279bf2896527b09c
cryspycreme <crystalchiamm@gmail.com>
2025-11-05 12:38:50 +1100

    Reflog to existing commit - create table and map attributes, implement Amenity Repo, refactor facade

--- part3/app/models/amenity.py
+++ part3/app/models/amenity.py
@@ -1,5 +1,5 @@
+from app.models.base_model import BaseModel, validates
 from app import db
-from app.models.base_model import BaseModel
 
 class Amenity(BaseModel):
     __tablename__ = 'amenities'
@@ -12,41 +12,72 @@ class Amenity(BaseModel):
         self.name = name
         self.description = description
 
-    """Getters and Setters"""
-    """Amenity name"""
-    @property
-    def name(self):
-        return self._name
-    
-    @name.setter
-    def name(self, value):
+    @validates("name")
+    def validate_name(self, key, value):
+        # 'name' valdiation and save
         if not isinstance(value, str):
             raise TypeError("Amenity name must be a string")
         
         value = value.strip()
-        if len(value) > 0 and len(value)<= 50:
-            self._name = value
-        elif len(value) <= 0:
+        if len(value) <= 0:
             raise ValueError("Amenity name must not be empty")
         elif len(value) > 50:
             raise ValueError("Amenity name cannot exceed 50 characters")
-
-    """Amenity description"""
-    @property
-    def description(self):
-        return self._description
-    
-    @description.setter
-    def description(self, value):
+        
+        return value
+        
+    @validates("description")
+    def validate_description(self, key, value):
+    # 'description' validation and save
         if value is None:
-            self._description = None
-            return
+            return None
         
         if not isinstance(value, str):
             raise TypeError("Amenity description must be a string")
         
         value = value.strip()
-        if len(value) > 0 and len(value)<= 100:
-            self._description = value
-        else:
-            raise ValueError("Amenity description cannot exceed 100 characters")
+        if len(value) <= 0:
+            raise ValueError("Amenity description must not be empty")
+        elif len(value) > 50:
+            raise ValueError("Amenity description cannot exceed 50 characters")
+        
+        return value
+
+    # """Getters and Setters"""
+    # """Amenity name"""
+    # @property
+    # def name(self):
+    #     return self._name
+    
+    # @name.setter
+    # def name(self, value):
+    #     if not isinstance(value, str):
+    #         raise TypeError("Amenity name must be a string")
+        
+    #     value = value.strip()
+    #     if len(value) > 0 and len(value)<= 50:
+    #         self._name = value
+    #     elif len(value) <= 0:
+    #         raise ValueError("Amenity name must not be empty")
+    #     elif len(value) > 50:
+    #         raise ValueError("Amenity name cannot exceed 50 characters")
+
+    # """Amenity description"""
+    # @property
+    # def description(self):
+    #     return self._description
+    
+    # @description.setter
+    # def description(self, value):
+    #     if value is None:
+    #         self._description = None
+    #         return
+        
+    #     if not isinstance(value, str):
+    #         raise TypeError("Amenity description must be a string")
+        
+    #     value = value.strip()
+    #     if len(value) > 0 and len(value)<= 100:
+    #         self._description = value
+    #     else:
+    #         raise ValueError("Amenity description cannot exceed 100 characters")
