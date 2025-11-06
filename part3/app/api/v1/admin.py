f7b330bd3b4a3f0c2a95db4965318c16689fe1c3
cryspycreme <crystalchiamm@gmail.com>
2025-11-06 15:54:02 +1100

    Update amenities endpoint for admin

--- part3/app/api/v1/admin.py
+++ part3/app/api/v1/admin.py
@@ -160,35 +160,3 @@ class AdminAmenityModify(Resource):
                 return {'error':error_message}, 404
             else:
                 return {'error': 'An unexpected error occurred'}, 500
-
-
-# Allow Admins to Bypass Ownership Restrictions
-@api.route('/places/<place_id>')
-class AdminPlaceModify(Resource):
-    
-    @jwt_required()
-    def put(self, place_id):
-        current_user = get_jwt()
-
-        # Set is_admin default to False if not exists
-        is_admin = current_user.get('is_admin', False)
-        user_id = current_user.get('id')
-
-        place = facade.get_place(place_id)
-        if not is_admin and place.owner_id != user_id:
-            return {'error': 'Unauthorized action'}, 403
-
-        # Logic to update the place
-        data = api.payload or {}
-        current_user = get_jwt()
-        # retrieve place
-        place = facade.get_place(place_id)
-        if not place:
-            return {'error': 'Place not found'}, 404
-        try:
-            update_place = facade.update_place(place_id, data)
-            if update_place.owner_id != current_user:
-                return {'error': 'Unauthorised action'}, 403
-            return update_place, 200
-        except ValueError as e:
-            return {"error:", str(e)}, 404
