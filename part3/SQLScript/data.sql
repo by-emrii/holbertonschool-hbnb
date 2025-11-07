ca00c067fb6021a1c6b7d03853a84031097d2aae
GraceKayem <11557@holbertonstudents.com>
2025-11-07 11:40:00 +1100

    save

--- part3/SQLScript/data.sql
+++ part3/SQLScript/data.sql
@@ -20,7 +20,7 @@ CREATE TABLE IF NOT EXISTS "users" (
 CREATE TABLE IF NOT EXISTS "places" (
     id CHAR(36) PRIMARY KEY, -- uuid
     title VARCHAR(255) NOT NULL,
-    description TEXT,
+    description TEXT NOT NULL,
     price DECIMAL(10, 2) NOT NULL,
     latitude FLOAT NOT NULL,
     longitude FLOAT  NOT NULL,
