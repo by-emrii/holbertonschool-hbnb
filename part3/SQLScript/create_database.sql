987867a9e4ae41c30023d502d22dbebd0f55bfa7
Emily Chew <emilychewsh@gmail.com>
2025-11-11 17:15:57 +1100

    add seed data of admin user

--- part3/SQLScript/create_database.sql
+++ part3/SQLScript/create_database.sql
@@ -77,7 +77,7 @@ CREATE TABLE IF NOT EXISTS place_amenity (
 -- ==========================
 -- Administrator User: 
 -- ==========================
-INSERT users (id, first_name, last_name, email, password, is_admin) VALUES (
+INSERT INTO users (id, first_name, last_name, email, password, is_admin) VALUES (
     '36c9050e-ddd3-4c3b-9731-9f487208bbc1',
     'Admin',
     'HBnB',
